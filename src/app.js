3b61336fa70bb26c7e82af2d11022957254bdc24
szaker1990 <szaker1990@github.com>
2020-07-17 10:24:52 +0200

    Basic Setup

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './scss/main.scss';
+
+const App = () =>{
+    return <h1>Hello Shop</h1>
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('app')
+)
---
+++ src/components/MainPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const MainPage = () =>{
+
+    return(
+        <>
+            <h1>Witaj w Aplikacji Zakupowej</h1>
+
+            </>
+    )
+
+}
